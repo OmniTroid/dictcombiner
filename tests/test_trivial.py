4c03f926415b1de2d34d8ee2ddd41fddbdd2c7d5
David Skoland <davidskoland@gmail.com>
2021-09-18 16:35:42 +0200

    add combine dict test

--- tests/test_trivial.py
+++ tests/test_trivial.py
@@ -2,11 +2,48 @@ import pytest
 import os
 from pathlib import Path
 
-import yamlcombiner
+
+import yamlcombiner as yc
 
 def test_trivial():
 	script_dir = Path(__file__).parent.resolve()
 	base_yml = Path(script_dir, 'testdata', 'base.yml')
 	sub_yml_dir = Path(script_dir, 'testdata', 'sub')
-	yc = yamlcombiner.YamlCombiner(base_yml, sub_yml_dir)
+	combiner = yc.YamlCombiner(base_yml, sub_yml_dir)
 
+def test_combine_dicts():
+	base_dict = {
+		'config': 15,
+		'settings': {
+			'a': 1,
+			'b': 2,
+			'c': 3
+		}
+	}
+
+	sub_dicts = [
+	{
+		'settings': {
+			'a': 2,
+			'b': 3
+		}
+	},
+	{
+		'settings': {
+			'a': 5
+		}
+	}
+	]
+
+	expected_result_dict = {
+		'config': 15,
+		'settings': {
+			'a': 5,
+			'b': 3,
+			'c': 3
+		}
+	}
+
+	result_dict = yc.utils.combine_dicts(base_dict, sub_dicts)
+
+	assert result_dict == expected_result_dict
